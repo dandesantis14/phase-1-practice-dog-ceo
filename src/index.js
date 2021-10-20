ee7b69b14962d0168b0425bd9a7af1855b8064c7
Daniel <dandesantis14@gmail.com>
2021-10-20 13:49:16 -0400

    Trying for the filter ability

--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
 console.log('%c HI', 'color: firebrick')
+
+window.addEventListener('DOMContentLoaded',() => {
+    fetchDogPictures()
+    fetchDogBreeds()
+})
+
+const fetchDogPictures = () => {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then (response => response.json())
+        .then ((json) => {
+            console.log(json)
+            json.message.forEach(el => makeImg(el));
+        })
+}
+
+const fetchDogBreeds = () => {
+    fetch ('https://dog.ceo/api/breeds/list/all')
+        .then (res => res.json())
+        .then ((json) => Object.keys(json.message).forEach(el => makeList(el)))
+        .then (() => {
+            addColorChange()
+            dropdownListener()
+        })
+}
+
+    // Makes image element to be appended to DOM
+const makeImg = img => {
+    let newImg = document.createElement('img');
+    newImg.src = img;
+    addEl(newImg);
+}
+    // Appends any elements passed to it to the dog container of document
+const addEl = el => document.getElementById('dog-image-container').append(el);
+
+
+//Adds listener for filter change
+const dropdownListener = () => {
+    const breedDropdown = document.getElementById('breed-dropdown')
+    breedDropdown.addEventListener('change', () => {
+        const filterValue = breedDropdown.value;
+        filterBreeds(filterValue);
+    })
+}
+
+//clears existing breed list for filtering
+const removeList = (parent) => {
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//creates new list based on passed filter value
+const filterBreeds = (filterValue) => {
+    const parent = document.getElementById('dog-breeds');
+    removeList(parent);
+    
+}
+
+    // Creates list elements
+
+const makeList = element => {
+    let newEntry = document.createElement('li');
+    newEntry.textContent = element;
+    document.getElementById('dog-breeds').append(newEntry);
+}
+    
+    // Applies event listeners that allow for color change
+const addColorChange = () => {
+    const listElements = document.querySelectorAll('li')
+    listElements.forEach(el => {
+        el.addEventListener('click', () => {
+            el.style.color = 'teal'}) 
+    })
+}
+
+
+
+
+
+
