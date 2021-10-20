528145ddf28acba0d99c886514c24a7ca6b27785
Daniel <dandesantis14@gmail.com>
2021-10-20 14:26:58 -0400

    got the filter working

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded',() => {
     fetchDogBreeds()
 })
 
+//for dog images
 const fetchDogPictures = () => {
     fetch("https://dog.ceo/api/breeds/image/random/4")
         .then (response => response.json())
@@ -14,6 +15,7 @@ const fetchDogPictures = () => {
         })
 }
 
+//For initial breed list
 const fetchDogBreeds = () => {
     fetch ('https://dog.ceo/api/breeds/list/all')
         .then (res => res.json())
@@ -24,17 +26,30 @@ const fetchDogBreeds = () => {
         })
 }
 
-    // Makes image element to be appended to DOM
+//For displaying filtered breeds list
+const fetchDogBreedsFiltered = (filterValue) => {
+    fetch ('https://dog.ceo/api/breeds/list/all')
+        .then (res => res.json())
+        .then ((json) => {
+            const filteredBreeds = Object.keys(json.message).filter(el => el.charAt(0) === filterValue)
+            filteredBreeds.forEach(el => makeList(el))
+        })
+        .then (() => {
+            addColorChange()
+        })
+}
+
+// Makes image element to be appended to DOM
 const makeImg = img => {
     let newImg = document.createElement('img');
     newImg.src = img;
     addEl(newImg);
 }
-    // Appends any elements passed to it to the dog container of document
+
+// Appends any elements passed to it to the dog container of document
 const addEl = el => document.getElementById('dog-image-container').append(el);
 
-
-//Adds listener for filter change
+// Adds listener for filter change
 const dropdownListener = () => {
     const breedDropdown = document.getElementById('breed-dropdown')
     breedDropdown.addEventListener('change', () => {
@@ -43,29 +58,28 @@ const dropdownListener = () => {
     })
 }
 
-//clears existing breed list for filtering
+// Clears existing breed list for filtering
 const removeList = (parent) => {
     while (parent.firstChild){
         parent.removeChild(parent.firstChild);
     }
 }
 
-//creates new list based on passed filter value
+// Creates new list based on passed filter value
 const filterBreeds = (filterValue) => {
     const parent = document.getElementById('dog-breeds');
     removeList(parent);
-    
+    fetchDogBreedsFiltered(filterValue)   
 }
 
-    // Creates list elements
-
+// Creates list elements
 const makeList = element => {
     let newEntry = document.createElement('li');
     newEntry.textContent = element;
     document.getElementById('dog-breeds').append(newEntry);
 }
     
-    // Applies event listeners that allow for color change
+// Applies event listeners that allow for color change
 const addColorChange = () => {
     const listElements = document.querySelectorAll('li')
     listElements.forEach(el => {
